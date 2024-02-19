a73e4610b63b38049c65f4da16681302d8112202
mithilesh <connectmk@hotmail.com>
2024-02-19 20:18:09 +0530

    sequilize implmenetation

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const User = require('./models/user');
 const Product = require('./models/product');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 
 const app = express();
 
@@ -43,6 +46,11 @@ Cart.belongsTo(User);
 
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+
+
 
 sequelize
 //.sync({ force: true })
@@ -56,8 +64,12 @@ sequelize
     }
     return user;
 })
-.then(result => {
+.then(user =>{
+    return user.createCart();
+})
+.then(user => {
     app.listen(3000);
-}).catch(err => console.log(err));
+})
+.catch(err => console.log(err));
 
 
